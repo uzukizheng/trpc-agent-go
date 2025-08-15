e5cea88b8534658423401b60370cb82dcfec05a8
ChenMingjie <chenmingjie0828@163.com>
2025-08-15 11:34:20 +0800

    Docs: update license (#217)
    Update all License file

--- examples/telemetry/agent/agent.go
+++ examples/telemetry/agent/agent.go
@@ -1,11 +1,8 @@
 // Tencent is pleased to support the open source community by making trpc-agent-go available.
 //
-// Copyright (C) 2025 Tencent.
-// All rights reserved.
-//
-// If you have downloaded a copy of the tRPC source code from Tencent,
-// please note that tRPC source code is licensed under the  Apache 2.0 License,
-// A copy of the Apache 2.0 License is included in this file.
+// Copyright (C) 2025 Tencent.  All rights reserved.
+
+// trpc-agent-go is licensed under the Apache License Version 2.0.
 package agent
 
 import (
